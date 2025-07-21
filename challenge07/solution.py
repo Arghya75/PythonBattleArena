bc7e04876dd3c6907e3bed24c5210467268ac955
Arghya Chakrborty <arghya.chakraborty.7503@gmail.com>
2025-07-21 22:59:54 +0600

    Challenge 7 completed

---
+++ challenge07/solution.py
@@ -0,0 +1,8 @@
+# Taking user input
+userInput = input("Enter your string: ").strip()
+
+# Replacing the space with hypens
+replacedString = userInput.replace(' ', '-')
+
+# printing replaced string
+print(f"Modified String: {replacedString}")
